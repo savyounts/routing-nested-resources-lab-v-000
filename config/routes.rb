096c6ce1682be18214cc03b4d99f2280ad6dd7e7
Savannah Younts <savyounts@gmail.com>
2018-09-27 17:23:43 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :artists do
-    resouces :songs, only: [:index, :show]
+    resources :songs, only: [:index, :show]
   end
-  
+
   resources :songs
 end
